c88f1e34488ec78bbe0d21a09d9eb1ad0bf9ea5b
Miku Shimizu <miku@MikunoMacBook-Pro.local>
2017-04-07 17:49:27 +0900

    first commit

---
+++ FirebaseSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirebaseSample
+//
+//  Created by Miku Shimizu on 2017/04/05.
+//  Copyright © 2017年 Miku Shimizu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
